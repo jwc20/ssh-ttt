c3a2dce85d408549a58c1d57c6372826cb79632b
jw <choi.jae100@gmail.com>
2026-02-05 13:09:05 -0800

    add position and unit test

---
+++ position_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestTicTacToe(t *testing.T) {
+	t.Run("test init", func(t *testing.T) {
+		position := initPosition()
+
+		assertEqual(t, position.Board, strings.Repeat(" ", 9))
+		assertEqual(t, position.Turn, "x")
+	})
+}
+
+func assertEqual(t *testing.T, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
