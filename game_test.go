53e2ceb1af29c6a4a7a48e0bf6fda59e46d5c15f
jw <choi.jae100@gmail.com>
2026-02-06 10:34:41 -0800

    add basic test

--- game_test.go
+++ game_test.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGame(t *testing.T) {
+	game := initGame()
+
+	t.Run("test init", func(t *testing.T) {
+		assert.Equal(t, game.board, "         ")
+	})
+}
