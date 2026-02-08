ad96e081d709dd0e3a573721648c6e7cc73c4c59
jw <choi.jae100@gmail.com>
2026-02-08 12:38:39 -0800

    refactor

--- cmd/webserver/webserver.go
+++ cmd/webserver/webserver.go
@@ -1,52 +1,26 @@
 package main
 
 import (
-	"database/sql"
 	"log"
 
 	"github.com/gin-gonic/gin"
+	ttt "github.com/jwc20/ssh-ttt"
 	"github.com/jwc20/ssh-ttt/handlers"
 	_ "github.com/mattn/go-sqlite3"
 )
 
-var db *sql.DB
-
-func initDB() {
-	var err error
-	db, err = sql.Open("sqlite3", "./app.db")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	createUsersTable := `CREATE TABLE IF NOT EXISTS users (
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		name TEXT NOT NULL,
-		public_key TEXT NOT NULL,
-		created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-	);`
-
-	_, err = db.Exec(createUsersTable)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	createRoomsTable := `CREATE TABLE IF NOT EXISTS rooms (
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		name TEXT NOT NULL,
-		winner TEXT NOT NULL,
-		created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-		finished_at DATETIME
-	);`
-
-	_, err = db.Exec(createRoomsTable)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
+const dbFileName = "/.app.db"
 
 func main() {
-	initDB()
+	store, close, err := ttt.FileSystemTTTStoreFromFile(dbFileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer close()
+
 	router := gin.Default()
+
+	db := store.Database
 
 	router.GET("/users", handlers.ListUser(db))
 	router.POST("/users", handlers.CreateUser(db))
